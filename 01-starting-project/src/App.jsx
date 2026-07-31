2c6d958a05a6d1b977bfabb46a915097c66c4931
Smit Patel <smitp8720@gmail.com>
2026-07-31 13:59:58 -0700

    practiced how to create components and dynamic variables to write easily maintainable code

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,27 @@
+import reactImg from "./assets/react-core-concepts.png"
+
+const reactDescriptions = ["Core", "Crucial", "Fundamental"];
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * max+1);
+}
+
+function MyHeader() {
+  const descriptions = reactDescriptions[genRandomInt(2)]
+  return(<header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {descriptions} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>)
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <MyHeader/> 
       <main>
         <h2>Time to get started!</h2>
       </main>
