e6c397f5556fc1154d81d3359602c3608928769d
Smit Patel <smitp8720@gmail.com>
2026-08-03 15:26:14 -0700

    Understanding how to use props and deconstruct objects

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png"
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Core", "Crucial", "Fundamental"];
 
@@ -18,11 +19,31 @@ function MyHeader() {
   </header>)
 }
 
+function CoreConcepts({image, title, description}) {
+
+  return (
+    <li>
+      <img src = {image} alt ={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <MyHeader/> 
       <main>
+        <section id ="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 React Basics
 
-only need to run npm install the first time you create the project to get all dependencies
-to run the project use: npm run dev
+# only need to run npm install the first time you create the project to get all dependencies
+
+# to run the project use: npm run dev
+
+# You can use Components to create more maintainable code
+- the component needs to be Uppercase and must have the "export" tag infront inorder to use in a different file
+
+# jsx files allow you to write html in js files that would usually give you an error
+
+# in order to use dynamic variables in your html code of your jsx file: need to use curly braces
+
+# import images at the top and use the alias so it doesn't break during build/production
+
+# Props are custom html attributes set onto Componenets
+
+# named exports need to be imported with curlly braces 
